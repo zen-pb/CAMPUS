8c1504dbb97f3d519bf8a68f763600cb9978518e
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-10-05 21:41:19 +0800

    Add session

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { Login, Signup, Dashboard } from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate();
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    if (session) {
+      sessionStorage.setItem("session", JSON.stringify(session));
+    }
+  }, [session]);
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const sessionData = sessionStorage.getItem("session");
+      setSession(sessionData ? JSON.parse(sessionData) : null);
+    };
+
+    handleStorageChange();
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={
+            session ? (
+              <Navigate to="/dashboard" />
+            ) : (
+              <Login setSession={setSession} />
+            )
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/dashboard"
+          element={
+            session ? (
+              <Dashboard session={session} setSession={setSession} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
