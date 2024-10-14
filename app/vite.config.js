654d2db042fc737fc5210f5e6dbac2c13eff686d
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-10-14 17:05:46 +0800

    Change configuration, add other dependencies

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['mock-aws-s3', 'aws-sdk', 'nock'],
+      output: {
+        globals: {
+          'mock-aws-s3': 'mockAwsS3',
+          'aws-sdk': 'AWS',
+          'nock': 'nock',
+        },
+      },
+    },
+  },
+});
