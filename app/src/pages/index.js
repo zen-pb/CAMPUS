c3705c4a1a583cdc451a0b04dd62859f4bab0741
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-10-23 19:37:59 +0800

    Make the imports alphabetical

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,7 +1,7 @@
-import Login from "./Login";
-import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import ForgotPassword from "./ForgotPassword";
+import Login from "./Login";
 import ResetPassword from "./ResetPassword";
+import Signup from "./Signup";
 
-export { Dashboard, ForgotPassword, ResetPassword, Login, Signup };
+export { Dashboard, ForgotPassword, Login, ResetPassword, Signup };
